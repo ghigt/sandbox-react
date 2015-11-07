8ba465a95905795c0aa5581be659932cd3f76e41
Ghislain Guiot <gt.ghislain@gmail.com>
2015-11-07 17:06:39 +0100

    finish the voting server

---
+++ redux-tutorial/voting-server/src/server.js
@@ -0,0 +1,19 @@
+import Server from 'socket.io';
+
+export default function startServer() {
+	const io = new Server().attach(8090);
+
+	store.subscribe(() => {
+			console.log('new suscribtion') // Debug
+			io.emit('state', store.getState().toJS());
+		}
+	);
+
+	io.on('connection', (socket) => {
+		console.log('new connection') // Debug
+		socket.emit('state', store.getState().toJS());
+		// Here should have sort of firewall or authentication mechanism
+		// to do not allow any client to broadcast events.
+		socker.on('action', store.dispatch.bind(store));
+	})
+}
